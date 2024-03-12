0a97f382693e4254e27764e366fece617eb5ff1f
Ac-ayush <100469226+Ac-ayush@users.noreply.github.com>
2024-03-12 04:23:20 +0000

    jdfja

---
+++ 03_basics/02_scops.js
@@ -0,0 +1,54 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+//NOTE : This is called Closers: chilld function can use properties of parent function
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+//++++++++++++++++++++++++++++++++++++++++INTERSTING++++++++++++++++++++++++++++++++++++++
+console.log(addone(5))      //<--valid
+
+function addone(num){
+    return num + 1
+}
+
+
+
+// addTwo(5)           //<-- Expression :: invalid because iss baar function variable me store he 
+const addTwo = function(num){
+    return num + 2
+}
+
+console.log(addTwo(66))
